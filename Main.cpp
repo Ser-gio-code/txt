e5b90ca03df5f37a16d70ba7304c60e6dfcc224a
ser-gio-code <sergio_javi@outlook.com>
2020-04-05 21:44:11 -0300

    add c++ file

---
+++ Main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main () {
+
+string texto;
+
+cout << "escriba su texto" << endl;
+cin >> texto;
+
+cout << "hola, su texto es" << texto << endl;
+
+
+
+
+cin.ignore () ;
+cin.get () ;
+ 
+
+
+return 0 ;
+
+}
+
